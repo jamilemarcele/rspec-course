49c44efb1e690e06c9d210ad89cb81f11472a46c
Jamile Radloff <jamilemarcele@gmail.com>
2021-05-07 11:02:41 +0200

    nested describe

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -8,20 +8,20 @@ class Card
 end
 
 RSpec.describe Card do
-  # before do
-  #   @card = Card.new('Ace', 'Spades')
-  # end
   let(:card) { Card.new('Ace', 'Spades') }
 
   # specify 'has a type'
   it 'has a rank' do
     expect(card.rank).to eq('Ace')
-    # card.rank = 'Queen'
-    # expect(card.rank).to eq('Queen')
   end
 
   it 'has a suit' do
     expect(card.suit).to eq('Spades')
+  end
+
+  it 'has a custom error message' do
+    comparison = 'Spades'
+    expect(card.suit).to eq(comparison), "Hey, I expected #{comparison}, but I got #{card.suit} instead!"
   end
 
 end
