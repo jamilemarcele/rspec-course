8f39d59bdeba0f4bd8b58f91420089c7ce4d1718
Jamile Radloff <jamilemarcele@gmail.com>
2021-05-07 11:10:04 +0200

    using context instead describe

--- spec/context_spec.rb
+++ spec/context_spec.rb
@@ -1,14 +1,15 @@
 RSpec.describe '#even? method' do
   # it 'should return true if number is even'
   # it "should return false if number is odd"
+  # context and describe are the same
 
-  describe 'with even number' do
+  context 'with even number' do
     it 'should return true' do
       expect(4.even?).to eq(true)
     end
   end
 
-  describe 'with odd number' do
+  context 'with odd number' do
     it 'should return false' do
       expect(5.even?).to eq(false)
     end
